77a9cee39be31903644b4772e27b3e582e68ed0d
Donovan Schaafsma <donovan@Donovans-MacBook-Pro.local>
2022-04-08 09:09:09 +0200

    Generated angular components from stencil

--- stenciljs-example/stencil.config.ts
+++ stenciljs-example/stencil.config.ts
@@ -1,8 +1,14 @@
 import { Config } from '@stencil/core';
+import { angularOutputTarget } from '@stencil/angular-output-target';
 
 export const config: Config = {
   namespace: 'stenciljs-example',
   outputTargets: [
+    angularOutputTarget({
+      componentCorePackage: 'stenciljs-example',
+      directivesProxyFile: '../angular-workspace/projects/component-library/src/lib/stencil-generated/components.ts',
+      directivesArrayFile: '../angular-workspace/projects/component-library/src/lib/stencil-generated/index.ts',
+    }),
     {
       type: 'dist',
       esmLoaderPath: '../loader',
